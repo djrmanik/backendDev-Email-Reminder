c4e95691414fee1284ec1cb681deae3280cb91bc
djrmanik <djuharghm@gmail.com>
2025-08-01 23:31:12 +0700

    Create authentication (signUp, signIn) & routes v2

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
+import { signUp, signIn, signOut } from '../controllers/auth.controllers.js';
 
 const authRouter = Router();
 
-authRouter.post('/', (req, res) => res.send({ title: 'Sign Up' }));
-authRouter.post('/', (req, res) => res.send({ title: 'Sign In' }));
-authRouter.post('/', (req, res) => res.send({ title: 'Sign Out' }));
+authRouter.post('/sign-up', signUp);
+authRouter.post('/sign-in', signIn);
+authRouter.post('/sign-out', signOut);
 
 export default authRouter;
