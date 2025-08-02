e690c9bce1d55c456fc114f06b6abd1bd245dd73
djrmanik <djuharghm@gmail.com>
2025-08-02 11:18:18 +0700

    Arcjet rate limiter configuration

--- app.js
+++ app.js
@@ -7,11 +7,13 @@ import authRouter from './routes/auth.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
 import connectToDatabase from './database/mongodb.js';
 import errorMiddleware from './middlewares/error.middleware.js';
+import arcjetMiddleware from './middlewares/arcjet.middleware.js';
 
 const app = express();
 app.use(express.json()); // alow json data sent in request
 app.use(express.urlencoded({ extended: false})); // process the form data sent by html form in a simple format
 app.use(cookieParser()); // reads cookies from incoming request to store user data
+app.use(arcjetMiddleware)
 
 
 app.use('/api/v1/auth', authRouter);
