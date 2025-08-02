60b3494e21d82891abda43ac64a6ad67245ef0ad
djrmanik <djuharghm@gmail.com>
2025-08-02 12:33:47 +0700

    create subscription controller & fix bug renewalDate

--- models/subscription.models.js
+++ models/subscription.models.js
@@ -47,7 +47,7 @@ const subscriptionSchema = new mongoose.Schema({
     },
     renewalDate: {
         type: Date,
-        required: true,
+        required: false,
         validate: {
             validator: function (value) {
                 return value > this.startDate;
--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import authorize from '../middlewares/auth.middleware.js';
+import { createSubscription } from '../controllers/subscription.controller.js';
 
 const subscriptionRouter = Router();
 
@@ -6,7 +8,7 @@ subscriptionRouter.get('/', (req, res) => res.send({title: 'Get all subscription
 
 subscriptionRouter.get('/:id', (req, res) => res.send({title: 'Get subscription details'}));
 
-subscriptionRouter.post('/', (req, res) => res.send({title: 'Create subscription'}));
+subscriptionRouter.post('/', authorize, createSubscription);
 
 subscriptionRouter.put('/:id', (req, res) => res.send({title: 'Update subscription'}));
 
