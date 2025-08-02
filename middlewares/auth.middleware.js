9c23a8d946fb0315652b3744251c18e3e3e922d2
djrmanik <djuharghm@gmail.com>
2025-08-02 10:47:25 +0700

    authorization v2

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,3 +1,27 @@
-const authorize = async (req, res, next) => {
+import jwt from 'jsonwebtoken';
+import { JWT_SECRET } from "../config/env.js";
+import User from "../models/user.models.js";
 
+// orang bikin req (get user details) -> authorize middle -> verify siapa yang ngelakuin -> valid / have permission? -> give access
+const authorize = async (req, res, next) => {
+    try {
+        let token;
+
+        if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+            token = req.headers.authorization.split(' ')[1];
+        }
+        if(!token) return res.status(401).json({ message: 'Unauthorized' });
+
+        const decoded = jwt.verify(token, JWT_SECRET);
+
+        const user = await User.findById(decoded.userId);
+        if(!user) return res.status(404).json({ message: 'Unauthorized' });
+        req.user = user;
+
+        next();
+    } catch (error) {
+        res.status(401).json({ message: 'Unauthorized', error: error.message });
+    }
 }
+
+export default authorize;
