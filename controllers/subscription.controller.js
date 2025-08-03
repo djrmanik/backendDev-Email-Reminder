a8649ebc5b70d8a3862d83a739697e806044c8e3
djrmanik <djuharghm@gmail.com>
2025-08-03 08:58:18 +0700

    fix another bug in subscription.controller.js

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -1,15 +1,15 @@
-import { workflowClient } from '../config/upstash.js';
-import Subscription from '../models/subscription.models.js';
-import { SERVER_URL } from '../config/env.js';
+import { workflowClient } from '../config/upstash.js'
+import Subscription from '../models/subscription.models.js'
+import { SERVER_URL } from '../config/env.js'
 
 export const createSubscription = async (req, res, next) => {
     try {
         const subscription = await Subscription.create({
             ...req.body,
-            user: req.user._id
-        })
+            user: req.user._id,
+        });
 
-        const {workflowRunId } = await workflowClient.trigger({
+        const { workflowRunId } = await workflowClient.trigger({
             url: `${SERVER_URL}/api/v1/workflows/subscription/reminder`,
             body: {
                 subscriptionId: subscription._id
@@ -20,7 +20,7 @@ export const createSubscription = async (req, res, next) => {
             retries: 0
         })
 
-        res.status(201).json({ success: true, data: subscription });
+        res.status(201).json({ success: true, data: {subscription, workflowRunId } });
     } catch (e) {
         next(e);
     }
