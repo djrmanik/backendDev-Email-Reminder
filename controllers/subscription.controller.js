cf62f459af3345f41ee66c5d746ffc415603ff58
djrmanik <djuharghm@gmail.com>
2025-08-03 08:42:43 +0700

    fix bug in subscription.controller.js

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -9,13 +9,15 @@ export const createSubscription = async (req, res, next) => {
             user: req.user._id
         })
 
-        await workflowClient.trigger({
+        const {workflowRunId } = await workflowClient.trigger({
             url: `${SERVER_URL}/api/v1/workflows/subscription/reminder`,
             body: {
-                requestPayLoad: {
-                    subscriptionId: subscription._id
-                }
-            }
+                subscriptionId: subscription._id
+            },
+            headers: {
+                'content-type': 'application/json'
+            },
+            retries: 0
         })
 
         res.status(201).json({ success: true, data: subscription });
@@ -26,12 +28,14 @@ export const createSubscription = async (req, res, next) => {
 
 export const getUsersSubscription = async (req, res, next) => {
     try {
-        if(req.user.id != req.params.id) {
+
+        if(req.user.id !== req.params.id) {
             const error = new Error('You are not the owner of this account');
             error.status = 401;
             throw error;
         }
         const subscription = await Subscription.find({ user: req.params.id });
+        
         res.status(200).json({ success: true, data: subscription });
     } catch (e) {
         next(e);
